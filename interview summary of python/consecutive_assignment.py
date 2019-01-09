d7f440253ba83e0cf03e8c6d409ebe76677dd5f1
haoran119 <34557994+haoran119@users.noreply.github.com>
2019-01-09 17:34:45 +1100

    Create consecutive_assignment.py

---
+++ interview summary of python/consecutive_assignment.py
@@ -0,0 +1,21 @@
+a = b = 0
+
+a = 1
+
+print(a)  # 1
+print(b)  # 0
+
+a = b = []
+
+a.append(0)
+
+print(a)  # [0]
+print(b)  # [0]
+
+a = []
+b = []
+
+a.append(0)
+
+print(a)  # [0]
+print(b)  # []
