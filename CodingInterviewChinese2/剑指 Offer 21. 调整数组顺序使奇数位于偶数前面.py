8e88213c55d285edee468f7f47831d02eca44bc8
haoran119 <34557994+haoran119@users.noreply.github.com>
2021-07-21 23:39:46 +1000

    Create 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面.py

---
+++ CodingInterviewChinese2/剑指 Offer 21. 调整数组顺序使奇数位于偶数前面.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  1 19:54:40 2019
+
+@author: Hao
+"""
+
+from typing import List
+
+
+class Solution:
+    # two points
+    # same idea as partition in quick sort
+    def exchange(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return nums
+
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            while left < right and nums[left] & 0x1 != 0:
+                left += 1
+            while left < right and nums[right] & 0x1 == 0:
+                right -= 1
+            if left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+
+        return nums
+
+mySolution = Solution()
+input = [1,2,3,4]
+result = mySolution.exchange(input)
+print(result)
