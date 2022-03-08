5d7d59997b7c47aeced1701ebcfa0ea195b3e2f8
haoran119 <34557994+haoran119@users.noreply.github.com>
2022-03-09 09:32:43 +1100

    Update quick_sort.cpp

--- 学习笔记之15道简单算法题/c++/quick_sort.cpp
+++ 学习笔记之15道简单算法题/c++/quick_sort.cpp
@@ -1,63 +1,63 @@
 #include <iostream>
 #include <vector>
 using namespace std;
- 
-class Solution 
+
+class Solution
 {
 public:
     // Pass in reference of vector parameter
     void quickSort(vector<int> &a, int inLeft, int inRight)
-    {       
-        cout << "Entry void qSort(vector<int> array, int left, int right)\n" << endl;
-        
-        if (inLeft >= inRight) 
-           return;
-        
-        int i = inLeft, j = inRight, temp = a[inLeft];
+    {
+        if (inLeft >= inRight)
+            return;
 
-        while (i < j)
-        {
-              // Scan from right to left
-              while ((i < j) && (a[j] >= temp)) 
-                    j --;
-              if (i < j) 
-                 a[i ++] = a[j];
-                 
-              // Scan from left to right
-              while ((i < j) && (a[i] <= temp)) 
-                    i ++;
-              if (i < j)
-                 a[j --] = a[i];                         
+        auto i = inLeft, j = inRight, temp = a.at(inLeft);
+
+        while (i < j) {
+            // Scan from right to left
+            while (i < j and a.at(j) >= temp) {
+                -- j;
+            }
+            if (i < j) {
+                a.at(i ++) = a.at(j);
+            }
+
+            // Scan from left to right
+            while (i < j and a.at(i) <= temp) {
+                ++ i;
+            }
+            if (i < j) {
+                a.at(j --) = a.at(i);
+            }
         }
-        // Base value temp is on the right position
-        a[i] = temp;
-        
+
+        // Base value temp is on the correct position now
+        a.at(i) = temp;
+
         // Divide and conquer
         quickSort(a, inLeft, i - 1);
-        quickSort(a, i + 1, inRight);        
+        quickSort(a, i + 1, inRight);
     }
 };
 
 int main ()
 {
-    Solution testSolution;
+    auto testSolution = Solution();
     int array[] = {0, 1, 5, 9, 0, 2, 6, 7, 8, 3, 4};
 //    int array[] = {0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 //    int array[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0};
 //    int array[] = {9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
     vector<int> vecArray(array, array + 11);
-    
-    for (vector<int>::const_iterator vecCIT = vecArray.begin(); vecCIT != vecArray.end(); vecCIT ++)
-        cout << *vecCIT << endl;
+
+    for (auto i : vecArray)
+        cout << i << " ";
     cout << endl;
 
     testSolution.quickSort(vecArray, 0, vecArray.size() - 1);
-        
-    for (vector<int>::const_iterator vecCIT = vecArray.begin(); vecCIT != vecArray.end(); vecCIT ++)
-        cout << *vecCIT << endl;
+
+    for (auto i : vecArray)
+        cout << i << " ";
     cout << endl;
-            
-    getchar();
-    
+
     return 0;
 }
